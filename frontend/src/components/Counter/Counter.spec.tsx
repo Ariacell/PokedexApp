0c2a2121bee9c3c6ef4cbbcedaf0e3db9e5e4cde
Aria <aria.jackson@outlook.com.au>
2019-09-17 23:00:34 +1000

    Added next test case and props models

--- frontend/src/components/Counter/Counter.spec.tsx
+++ frontend/src/components/Counter/Counter.spec.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { Props } from "react";
 import {render, fireEvent, waitForElement } from "@testing-library/react";
 
 import Counter from "./Counter";
+import { ICounterProps } from "./models";
+
+
+const mockProps: ICounterProps = {
+
+}
+
+//Props deprecated? need to figure out how to do this override with typescript correctly.
+function renderCounter(props: Partial<ICounterProps> = {}) {
+    const defaultProps: ICounterProps = {
+
+    }
+    return render(<Counter {...defaultProps}/>)
+}
 
 describe("Counter component ", () => {
     it('should display', () => {
-
+        renderCounter(mockProps);
     });
+
+    it('should start at 0', () => {
+        const { getByTestId } = renderCounter(mockProps);
+        const valueField = getByTestId('counter-value');
+        expect(valueField).toHaveTextContent('0');
+
+    })
 });
---
+++ frontend/src/components/Counter/Counter.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface ICounterProps {
+}
+
+const Counter: React.FC<ICounterProps> = (props) => {
+    return (
+      <div>
+        
+      </div>
+    );
+}
+export default Counter;
