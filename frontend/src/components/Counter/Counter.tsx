94625b028b0711ddf5c596a4f159050f2666535b
Aria <aria.jackson@outlook.com.au>
2019-09-18 13:49:21 +1000

    Made inc and dec button display test pass

--- frontend/src/components/Counter/Counter.tsx
+++ frontend/src/components/Counter/Counter.tsx
@@ -5,9 +5,13 @@ interface ICounterProps {
 
 const Counter: React.FC<ICounterProps> = (props) => {
     return (
-      <div data-testid="counter-value">
-        0
+        <>
+            <div data-testid="counter-value">
+                0
       </div>
+            <button data-testid="counter-increment">More</button>
+            <button data-testid="counter-decrement">Less</button>
+        </>
     );
 }
 export default Counter;
